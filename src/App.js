7304afa1b6b64b2c0c6bc7eedcd213ab909fa5ad
zemik1 <danilzemik73@gmail.com>
2023-11-27 04:06:50 +0500

    NavBar init

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import '../src/index.css'
+import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
+
 function App() {
     return (
-        <div className="App">
+        <div>
+            <NavBar/>
+            <Hero/>
         </div>
     );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,6 @@
+function Hero(){
+    return(
+        <div className='text-white'>Hero</div>
+    )
+}
+export default Hero
