e84678f1f639c6e92405dce89d425ee616576029
zemik1 <danilzemik73@gmail.com>
2023-11-27 08:02:14 +0500

    modal upd

--- src/components/uikit/UiModal.jsx
+++ src/components/uikit/UiModal.jsx
@@ -4,6 +4,7 @@ import thirdList from '../../assets/3.jpg'
 import {AiOutlineClose} from "react-icons/ai";
 
 function UiModal(props) {
+
     function closeModalHandler() {
         props.modal()
     }
@@ -11,11 +12,11 @@ function UiModal(props) {
     return (
         <div
             className='fixed hiden overflow-y-auto overflow-x-hidden outline-none inset-0 bg-gray-700/60 backdrop-blur pt-10 pb-10'>
-            <div className='bg-white rounded-lg w-[440px] flex flex-nowrap justify-center mx-auto p-10'>
+            <div className='fixed inset-0 overflow-y-auto bg-white rounded-lg md:w-[700px] sm:w-[390px] flex flex-nowrap justify-center mx-auto p-10'>
                 <div>
-                    <img src={oneList} alt='1'/>
-                    <img src={twoList} alt='2'/>
-                    <img src={thirdList} alt='3'/>
+                    <img src={oneList} alt="1"/>
+                    <img src={twoList} alt="2"/>
+                    <img src={thirdList} alt="3"/>
                 </div>
                 <div>
                     <AiOutlineClose onClick={closeModalHandler} size={30}/>
